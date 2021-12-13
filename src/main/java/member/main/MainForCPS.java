bc7d3e7ee6fe424bf92f7a2eb39ba45e4a320a2a
user <starkyumin0622@gmail.com>
2021-12-13 15:43:29 +0900

    ADD LOGGER

--- src/main/java/member/main/MainForCPS.java
+++ src/main/java/member/main/MainForCPS.java
@@ -1,5 +1,7 @@
 package member.main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import member.exception.IdPasswordNotMatchingException;
@@ -7,10 +9,13 @@ import member.exception.MemberNotFoundException;
 import member.service.ChangePasswordService;
 
 public class MainForCPS {
+	private final static Logger logger = LogManager.getLogger(MainForCPS.class);
 	public static void main(String[] args) {
+		logger.info("MainForCPS");
 		GenericXmlApplicationContext appctx = new GenericXmlApplicationContext("classpath:appctx.xml");
 		
 		ChangePasswordService cps = appctx.getBean("changePwdSvc", ChangePasswordService.class);
+		
 		try {
 			cps.ChangePassword("paraday@naver.com", "1234", "1111");
 			System.out.println("암호를 변경하였습니다");
